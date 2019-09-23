e7401ef2130fceeffea1decd8b75c38c4d847ba0
Gabbro <sergabbro@gmail.com>
2019-09-23 11:02:28 +0200

    ALPHA 1

---
+++ scripts/generator.js
@@ -0,0 +1,129 @@
+var consonantsDict = new Object();
+var strangerDict = new Object();
+var vocalsDict = new Object();
+
+consonantsDict = { "b" : 2, "c": 9, "d" : 8, 
+                    "f" : 2, "g": 3, "h" : 3, 
+                    "l" : 13, "m": 5, "n" : 14, 
+                    "p" : 6, "q": 1, "r" : 13, 
+                    "s" : 10, "t": 11, "v" : 4, 
+                    "z" : 1 };
+
+strangerDict = { "j" : 1, "k": 1, "w" : 1,
+                "x" : 3, "y": 1};
+
+vocalsDict = { "a" : 12, "e": 12, "i" : 11, 
+                "o" : 10, "u": 3 };
+
+var consonants = "";
+var stranger = "";
+var vocals = "";
+
+
+function initGen() {
+	setupStrings();
+
+	var _list = document.getElementById("list");
+	_list.innerHTML = "";
+
+	for(var i = 0; i < 100; i++) {
+
+		for (var i = 0; i < 100; i++) {
+			var _li = document.createElement("li");
+
+			_li.appendChild(document.createTextNode(getBestemmia()));
+
+			_list.appendChild(_li);
+		}
+
+	}
+
+}
+
+function getBestemmia() {
+	var doubleComboConsonantValue = 30;
+	var doubleConsonantValue = 60;
+	var doubleVocalValue = 90;
+	var strangerConsonantValue = 100;	
+
+	var memory = "";
+	var word = "";
+
+	var r = 0;
+	var wlength = Math.floor(Math.random() * 12) + 3;
+	for(var temp = 0; temp < wlength; temp++) {
+
+		r = Math.floor(Math.random() * doubleComboConsonantValue); 
+		if (r == doubleComboConsonantValue - 1 && word.length > 2 && word.length % 2 == 0) {
+
+			word += "gn" + " ";
+			memory = "";
+			continue;
+
+		}
+
+		r = Math.floor(Math.random() * doubleConsonantValue); 
+		if (r == doubleConsonantValue - 1 && word.length > 2 && word.length % 2 == 0) {
+
+			word += memory + " ";
+			memory = "";
+			continue;
+
+		}
+
+		r = Math.floor(Math.random() * doubleVocalValue); 
+		if (r == doubleVocalValue - 1 && word.length > 3 && word.length % 2 == 1) {
+
+			word += memory + " ";
+			memory = "";
+			continue;
+
+		}
+
+		r = Math.floor(Math.random() * strangerConsonantValue); 
+		if (r == strangerConsonantValue - 1 && word.length > 2 && word.length % 2 == 0) {
+
+			r = Math.floor(Math.random() * stranger.length); 
+			word += stranger.charAt(r);
+			continue;
+
+		}
+
+		if (word.length % 2 == 0) {
+
+			r = Math.floor(Math.random() * consonants.length); 
+			word += consonants.charAt(r);;
+			memory = consonants.charAt(r);
+
+		}
+		else {
+
+			r = Math.floor(Math.random() * vocals.length);  
+			word += vocals.charAt(r);
+			memory = vocals.charAt(r);
+
+		}
+
+
+	}
+
+	var bastemmia = "Dio " + word.replace(" ", "");
+
+	return bastemmia;
+}
+
+function setupStrings() {
+
+	for(var key in consonantsDict) 
+		for(var i = 0; i < consonantsDict[key]; i++)
+			consonants += key;
+
+	for(var key in strangerDict) 
+		for(var i = 0; i < strangerDict[key]; i++)
+			stranger += key;
+
+	for(var key in vocalsDict) 
+		for(var i = 0; i < vocalsDict[key]; i++)
+			vocals += key;
+
+}
