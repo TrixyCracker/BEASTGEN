9a3410e33902ca8babd0729ee7c608e72ee330c7
Gabbro <sergabbro@gmail.com>
2019-09-24 19:07:38 +0200

    updated values and code

--- scripts/generator.js
+++ scripts/generator.js
@@ -2,19 +2,25 @@ var consonantsDict = new Object();
 var strangerDict = new Object();
 var vocalsDict = new Object();
 
-consonantsDict = { "b" : 2, "c": 9, "d" : 8, 
-                    "f" : 2, "g": 3, "h" : 3, 
-                    "l" : 13, "m": 5, "n" : 14, 
-                    "p" : 6, "q": 1, "r" : 13, 
-                    "s" : 10, "t": 11, "v" : 4, 
-                    "z" : 1 };
+consonantsDict = {
+	"b": 2, "c": 9, "d": 8,
+	"f": 2, "g": 3, "h": 3,
+	"l": 13, "m": 5, "n": 14,
+	"p": 6, "q": 1, "r": 13,
+	"s": 10, "t": 11, "v": 4,
+	"z": 1
+};
 
-strangerDict = { "j" : 1, "k": 1, "w" : 1,
-                "x" : 3, "y": 1};
+strangerDict = {
+	"j": 1, "k": 1, "w": 1,
+	"x": 3, "y": 1
+};
 
-vocalsDict = { "a" : 12, "e": 12, "i" : 11, 
-				"o" : 10, "u": 3 };
-				
+vocalsDict = {
+	"a": 12, "e": 12, "i": 11,
+	"o": 10, "u": 3
+};
+
 
 
 var consonants = "";
@@ -28,105 +34,108 @@ function initGen() {
 	_list.innerHTML = "";
 
 
-		for (var i = 0; i < 25; i++) {
-			var _li = document.createElement("li");
+	for (var i = 0; i < 25; i++) {
+		var _li = document.createElement("li");
 
-			_li.appendChild(document.createTextNode(getBestemmia()));
+		_li.appendChild(document.createTextNode(getBestemmia()));
 
-			_list.appendChild(_li);
-		}
+		_list.appendChild(_li);
+	}
 
 
 
 }
 
 function getBestemmia() {
-	var doubleVocalValue = 20;
-	var doubleComboConsonantValue = 15;
-	var doubleConsonantValue = 10;
-	var strangerConsonantValue = 10;	
 
 	var memory = "";
 	var word = "";
 
+	var offset = Math.floor(Math.random() * 2);
+	if (offset) word = " ";
+
 	var r = 0;
 	var wlength = Math.floor(Math.random() * 10) + 3;
-	for(var temp = 0; temp < wlength; temp++) {
+	for (var temp = 0; temp < wlength; temp++) 
+	{
 
-		r = Math.floor(Math.random() * doubleComboConsonantValue); 
-		if (r == Math.round(doubleComboConsonantValue) && word.length > 2 && word.length % 2 == 0) {
+		if (word.length % 2 == 0) 
+		{
 
-			word += "gn" + " ";
-			memory = "";
-			continue;
+			r = Math.floor(Math.random() * 10);
 
-		}
+			if (word.length > 2 && r == 5) {
+				r = Math.floor(Math.random() * 3);
 
-		r = Math.floor(Math.random() * doubleConsonantValue); 
-		if (r == Math.round(doubleConsonantValue) && word.length > 2 && word.length % 2 == 0) {
+				switch(r) {
 
-			word += memory + " ";
-			memory = "";
-			continue;
+					case 0:
+						word += "gn" + " ";
+						memory = "";
+						break;
+					case 1: 
+						word += memory + " ";
+						memory = "";
+						break;
+					case 2:
+						r = Math.floor(Math.random() * stranger.length);
+						word += stranger.charAt(r);
+						break;
 
-		}
+				}
 
-		r = Math.floor(Math.random() * doubleVocalValue); 
-		if (r == Math.round(doubleVocalValue) && word.length > 3 && word.length % 2 == 1) {
+			}
+			else {
+				
+				r = Math.floor(Math.random() * consonants.length);
+				word += consonants.charAt(r);;
+				memory = consonants.charAt(r);
 
-			word += memory + " ";
-			memory = "";
-			continue;
-
-		}
-
-		r = Math.floor(Math.random() * strangerConsonantValue); 
-		if (r == Math.round(strangerConsonantValue) && word.length > 2 && word.length % 2 == 0) {
-
-			r = Math.floor(Math.random() * stranger.length); 
-			word += stranger.charAt(r);
-			continue;
-
-		}
-
-		if (word.length % 2 == 0) {
-
-			r = Math.floor(Math.random() * consonants.length); 
-			word += consonants.charAt(r);;
-			memory = consonants.charAt(r);
+			}		
 
 		}
-		else {
+		else 
+		{
 
-			r = Math.floor(Math.random() * vocals.length);  
-			word += vocals.charAt(r);
-			memory = vocals.charAt(r);
+			r = Math.floor(Math.random() * 200);
+			if (r == 50 && word.length > 2) {
+
+				word += memory + " ";
+				memory = "";
+
+			}
+			else {
+
+				r = Math.floor(Math.random() * vocals.length);
+				word += vocals.charAt(r);
+				memory = vocals.charAt(r);
+
+			}
 
 		}
-
 
 	}
 
 	word = word.replace(/\ /g, '');
 
-	var bastemmia = "Dio " + word;
+	var bestemmia = "Dio " + word;
 
-	return bastemmia;
+	return bestemmia;
 }
 
 function setupStrings() {
 
 
-	for(var key in consonantsDict) 
-		for(var i = 0; i < consonantsDict[key]; i++)
+	for (var key in consonantsDict)
+		for (var i = 0; i < consonantsDict[key]; i++)
 			consonants += key;
 
-	for(var key in strangerDict) 
-		for(var i = 0; i < strangerDict[key]; i++)
+	for (var key in strangerDict)
+		for (var i = 0; i < strangerDict[key]; i++)
 			stranger += key;
 
-	for(var key in vocalsDict) 
-		for(var i = 0; i < vocalsDict[key]; i++)
+	for (var key in vocalsDict)
+		for (var i = 0; i < vocalsDict[key]; i++)
 			vocals += key;
 
 }
